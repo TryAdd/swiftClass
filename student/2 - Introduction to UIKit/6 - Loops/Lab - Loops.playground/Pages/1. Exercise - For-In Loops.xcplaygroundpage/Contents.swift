51f25367362d9c657326aeec774884c73312b44c
Try Add <tryadd@Trys-MacBook-Pro.local>
2023-05-31 16:53:03 +0300

    done

--- student/2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
+++ student/2 - Introduction to UIKit/6 - Loops/Lab - Loops.playground/Pages/1. Exercise - For-In Loops.xcplaygroundpage/Contents.swift
@@ -3,14 +3,21 @@
  
  Create a for-in loop that loops through values 1 to 100, and prints each of the values.
  */
-
+for index in 1...100{
+    print("This is number \(index)")
+}
 
 //:  Create a for-in loop that loops through each of the characters in the `alphabet` string below, and prints each of the values alongside the index.
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
-
+for (index,letter) in alphabet.enumerated(){
+    print("\(index): \(letter)")
+}
 
 //:  Create a `[String: String]` dictionary, where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, then use a for-in loop to iterate over the pairs and print out the keys and values in a sentence.
-
+let capitals = ["Manama","Sitra","Sar"]
+for states in capitals{
+    print(states)
+}
 
 /*:
 page 1 of 6  |  [Next: App Exercise - Movements](@next)
