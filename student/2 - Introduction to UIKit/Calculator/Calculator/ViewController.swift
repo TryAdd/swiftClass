06702a6341f251ec12f8c8f1ad62f6b402c8f74a
Try Add <tryadd@Trys-MacBook-Pro.local>
2023-06-01 18:32:18 +0300

    done

---
+++ student/2 - Introduction to UIKit/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Try Add on 01/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
